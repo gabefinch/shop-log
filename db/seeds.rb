bdd0dcf1a9962f2afdff6285e0c4a735b8455342
Gabe Finch <gabefinch@gmail.com>
2015-06-08 12:29:40 -0700

    Add simple seed file for development, add bootswatch paper and other stock asset mods

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+jeffrey = User.create ({
+  card_number: 100,
+  first_name: 'Jeffrey',
+  last_name: 'Garmin'
+  })
+gabe = User.create ({
+  card_number: 200,
+  first_name: 'Gabe',
+  last_name: 'Finch'
+  })
+
+shared_index = Index.create ({number: 1001, external: false})
+
+smith_lab = Lab.create ({
+  name: 'Smith Lab',
+  pi_first: 'John',
+  pi_last: 'Smith',
+  users: [
+    jeffrey,
+    gabe
+    ],
+  indices: [
+    shared_index
+    ]
+  })
+jones_lab = Lab.create ({
+  name: 'Jones Lab',
+  pi_first: 'Delilah',
+  pi_last: 'Jones',
+  users: [
+    jeffrey,
+    gabe
+    ],
+  indices: [
+    shared_index
+    ]
+  })
