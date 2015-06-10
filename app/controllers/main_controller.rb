b0496cd25a9960bb28c19751ff6dae1932b047ed
Gabe Finch <gabefinch@gmail.com>
2015-06-10 12:12:09 -0700

    stopping point pre-kauai

---
+++ app/controllers/main_controller.rb
@@ -0,0 +1,8 @@
+class MainController < ApplicationController
+  def card_swipe
+    # if @user = User.find_by card_number(params[:card_number])
+    #   flash[:notice] = "Welcome " + @user.first_name + " " + @user.last_name + "."
+    #   render user#show
+    # end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
+  root :to => 'main#card_swipe'
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+  resources :entries, :only => [:index, :show, :new, :create, :edit, :update, :destroy]
   # Example resource route with options:
   #   resources :products do
   #     member do
