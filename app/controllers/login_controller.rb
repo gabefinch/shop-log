fd95a5ef1fe16173c2a5ea39710c47719da7516e
Gabe Finch <gabefinch@gmail.com>
2015-07-02 13:08:38 -0700

    early build out of user show 'home' page with flash for success in login

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,6 +2,7 @@ class LoginController < ApplicationController
   def cardswipe
     card_id = params[:card_id]
     @user = User.find_by card_number: card_id
+    flash[:notice] = "Login Succesful!"
     render 'users/show'
   end
 
