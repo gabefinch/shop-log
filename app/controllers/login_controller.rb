0f885b32de42da4bd3cf371d9d094ef25b4898a5
Gabe Finch <gabefinch@gmail.com>
2015-07-01 17:20:38 -0700

    change logic of login resources

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,8 +1,10 @@
-class MainController < ApplicationController
-  def card_swipe
+class LoginController < ApplicationController
+  def wait_for_swipe
+
+  end
+
     # if @user = User.find_by card_number(params[:card_number])
     #   flash[:notice] = "Welcome " + @user.first_name + " " + @user.last_name + "."
     #   render user#show
     # end
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  root :to => 'main#card_swipe'
+  root :to => 'login#wait_for_swipe'
+  resources :sessions
+  resources :users
+  resources :labs
+  resources :indices
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
   # Example of regular route:
