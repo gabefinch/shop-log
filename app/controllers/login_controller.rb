288178ad1c28ef7c01ec13ce22ca42ec9b0c5488
Gabe Finch <gabefinch@gmail.com>
2015-07-02 16:45:33 -0700

    build out and style user home/show

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,7 +2,7 @@ class LoginController < ApplicationController
   def cardswipe
     card_id = params[:card_id]
     @user = User.find_by card_number: card_id
-    flash[:notice] = "Login Succesful!"
+    flash[:notice] = "Logged in  as " + @user.first_name + " " + @user.last_name + "!"
     render 'users/show'
   end
 
