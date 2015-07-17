e92e7444b76c9afbd141fc586b5357869853966d
Gabe Finch <gabefinch@gmail.com>
2015-07-17 13:31:18 -0700

    Fix repeat flash notice appearance

--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -1,8 +1,11 @@
 class LoginController < ApplicationController
+  def wait_for_swipe
+    @logged_in = false
+  end
   def cardswipe
     card_id = params[:card_id]
     @user = User.find_by card_number: card_id
-    flash[:notice] = "Logged in  as " + @user.first_name + " " + @user.last_name + "!"
+    flash.now[:notice] = "Logged in  as " + @user.first_name + " " + @user.last_name + "!"
     render 'users/show'
   end
 
