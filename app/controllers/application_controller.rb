6570ef1edfdf2c53081537562c31375077ad6ee3
Gabe Finch <gabefinch@gmail.com>
2015-07-19 09:18:17 -0700

    Move user to current_user helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,10 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  helper_method :current_user
+
+  private
+  def current_user
+    @current_user ||= User.find_by card_number: session[:card_id] if User.find_by card_number: session[:card_id]
+  end
 end
--- app/controllers/login_controller.rb
+++ app/controllers/login_controller.rb
@@ -2,9 +2,8 @@ class LoginController < ApplicationController
   def wait_for_swipe
   end
   def cardswipe
-    card_id = params[:card_id]
-    @user = User.find_by card_number: card_id
-    flash.now[:notice] = "Logged in  as " + @user.first_name + " " + @user.last_name + "!"
+    session[:card_id] = params[:card_id]
+    # flash.now[:notice] = "Logged in  as " + @current_user.first_name + " " + @current_user.last_name + "!"
     render 'users/show'
   end
 
